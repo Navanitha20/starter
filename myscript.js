d47c5bc1f518a962c10120067c5d0402c43aecd8
Navanitha20 <navanitha20@gmail.com>
2016-12-07 00:45:05 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,12 +1,5 @@
 function loadDoc() {
-  var xhttp;
-  if (window.XMLHttpRequest) {
-    // code for modern browsers
-    xhttp = new XMLHttpRequest();
-    } else {
-    // code for IE6, IE5
-    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("demo").innerHTML = this.responseText;
